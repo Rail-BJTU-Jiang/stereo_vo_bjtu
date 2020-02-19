8a8650fe8274dc873bde66881d06b459fcb26568
HU Xiao <hux062303@gmail.com>
2020-02-19 12:41:55 +0100

    speed up nms

--- thirdparty/Stereo-Odometry-SOFT-master/code/functions/featureProcessing/nonMaximalSuppression/nonMaximumSuppression.m
+++ thirdparty/Stereo-Odometry-SOFT-master/code/functions/featureProcessing/nonMaximalSuppression/nonMaximumSuppression.m
@@ -34,44 +34,57 @@ keypoints = struct('location', {}, 'value', {}, 'class', {});
 for i = (nms_n + margin):(nms_n + 1):(height - nms_n - margin)
     for j = (nms_n + margin):(nms_n + 1):(width - nms_n - margin)
         % loop variables
-        f1min_i = i; f1min_j = j; f1max_i = i; f1max_j = j;
-        f2min_i = i; f2min_j = j; f2max_i = i; f2max_j = j;
+%         f1min_i = i; f1min_j = j; f1max_i = i; f1max_j = j;
+%         f2min_i = i; f2min_j = j; f2max_i = i; f2max_j = j;
         
         % get intensity values
-        f1min_val = I_f1(i, j);
-        f1max_val = f1min_val;
-        f2min_val = I_f2(i, j);
-        f2max_val = f2min_val;
+%         f1min_val = I_f1(i, j);
+%         f1max_val = f1min_val;
+%         f2min_val = I_f2(i, j);
+%         f2max_val = f2min_val;
         
         % Step a)
         % Partitions input image into blocks of sizes (n + 1) x (n + 1).
         % Search for maxima/minima within each block.
-        for i2 = i:(i + nms_n)
-            for j2 = j:(j + nms_n)
-                % for blob detector
-                currval = I_f1(i2, j2);
-                if (currval < f1min_val)         
-                    f1min_i   = i2;
-                    f1min_j   = j2;
-                    f1min_val = currval;
-                elseif (currval > f1max_val) 
-                    f1max_i   = i2;
-                    f1max_j   = j2;
-                    f1max_val = currval;
-                 end
-                % for corner detector
-                currval = I_f2(i2, j2);
-                if (currval < f2min_val)         
-                    f2min_i   = i2;
-                    f2min_j   = j2;
-                    f2min_val = currval;
-                elseif (currval > f2max_val) 
-                    f2max_i   = i2;
-                    f2max_j   = j2;
-                    f2max_val = currval;
-                 end
-            end
-        end
+%         tic
+        I1_block = I_f1(i:i+nms_n,j:j+nms_n);
+        I2_block = I_f2(i:i+nms_n,j:j+nms_n);
+        
+        [f1min_val, minid1] = min(I1_block(:));[f1min_i,f1min_j]=ind2sub(size(I1_block),minid1);f1min_i=f1min_i+i-1;f1min_j=f1min_j+j-1;
+        [f1max_val, minid2] = max(I1_block(:));[f1max_i,f1max_j]=ind2sub(size(I1_block),minid2);f1max_i=f1max_i+i-1;f1max_j=f1max_j+j-1;
+        [f2min_val, minid3] = min(I2_block(:));[f2min_i,f2min_j]=ind2sub(size(I1_block),minid3);f2min_i=f2min_i+i-1;f2min_j=f2min_j+j-1;
+        [f2max_val, minid4] = max(I2_block(:));[f2max_i,f2max_j]=ind2sub(size(I1_block),minid4);f2max_i=f2max_i+i-1;f2max_j=f2max_j+j-1;
+%         disp([[f1min_i,f1min_j],[f1max_i,f1max_j],[f2min_i,f2min_j],[f2max_i,f2max_j]]);
+%         toc;
+%         tic
+%         for i2 = i:(i + nms_n)
+%             for j2 = j:(j + nms_n)
+%                 % for blob detector
+%                 currval = I_f1(i2, j2);
+%                 if (currval < f1min_val)         
+%                     f1min_i   = i2;
+%                     f1min_j   = j2;
+%                     f1min_val = currval;
+%                 elseif (currval > f1max_val) 
+%                     f1max_i   = i2;
+%                     f1max_j   = j2;
+%                     f1max_val = currval;
+%                  end
+%                 % for corner detector
+%                 currval = I_f2(i2, j2);
+%                 if (currval < f2min_val)         
+%                     f2min_i   = i2;
+%                     f2min_j   = j2;
+%                     f2min_val = currval;
+%                 elseif (currval > f2max_val) 
+%                     f2max_i   = i2;
+%                     f2max_j   = j2;
+%                     f2max_val = currval;
+%                  end
+%             end
+%         end
+%         disp([[f1min_i,f1min_j],[f1max_i,f1max_j],[f2min_i,f2min_j],[f2max_i,f2max_j]]);
+%         toc;
         
         % Step b)
         % Test at the full neighborhood of candidate is tested. Elements 
